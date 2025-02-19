5e6bba13e931070fc863903dfb712a3293dab942
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-18 19:10:26 -0600

    Come on...

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import os
 os.system("git clone https://github.com/50-scratch-tabs/CanYouDecodeThisEngine")
+open("CanYouDecodeThisEngine/ciphers.txt","wb").write(open("ciphers.txt","rb").read())
 os.chdir("CanYouDecodeThisEngine")
 os.system("python3 -m CanYouDecodeThis.gen_html")
 os.rename("build/html/","../build")
