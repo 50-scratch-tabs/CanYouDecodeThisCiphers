ee03b727f186dd5dd8f21c982e1e686018e5bb1a
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-18 19:07:11 -0600

    Pretty please?

--- build.py
+++ build.py
@@ -2,4 +2,4 @@ import os
 os.system("git clone https://github.com/50-scratch-tabs/CanYouDecodeThisEngine")
 os.chdir("CanYouDecodeThisEngine")
 os.system("python3 CanYouDecodeThisEngine/gen_html.py")
-os.rename("build/html","../build")
+os.rename("build/html/","../build")
