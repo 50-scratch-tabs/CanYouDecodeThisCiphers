6797a05cfd3fd53f9695144d4a05e6756af746aa
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-02-18 19:18:22 -0600

    It might work this time

--- build.py
+++ build.py
@@ -4,3 +4,5 @@ open("CanYouDecodeThisEngine/ciphers.txt","wb").write(open("ciphers.txt","rb").r
 os.chdir("CanYouDecodeThisEngine")
 os.system("python3 -m CanYouDecodeThis.gen_html")
 os.rename("build/html/","../build")
+os.system("git add build")
+os.system('git commit -m "Automated build"')
